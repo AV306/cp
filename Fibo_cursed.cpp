3487f7add736faa6c085add6a756196115ee5fa4
Blender <66512356+AV306@users.noreply.github.com>
2023-11-16 15:15:27 +0800

    Update Fibo_cursed.cpp

--- Fibo_cursed.cpp
+++ Fibo_cursed.cpp
@@ -1,28 +1,34 @@
 
 #include <bits/stdc++.h>
-#define krys_free( ptr ) delete ptr; ptr = nullptr;
+#define do_8_times for ( int i = 2; i < 10; i++ )
+#define kymchi cin.tie( 0 ); cout.tie( 0 ); ios_base::sync_with_stdio( false );
+#define krystal_free( ptr ) delete ptr; ptr = nullptr;
 using namespace std;
 
 void generate_fibo( int* array )
 {
-    0[array](0);
-    1[array](1);
+    // first 2 fibo numbers
+    0[array] = 0;
+    1[array] = 1;
     
-    for ( int i = 2; i < 10; i++ )
-     i[array] = (i-1)[array] + (i-2)[array];
+    // rest of them
+    do_8_times i[array] = (i-1)[array] + (i-2)[array];
 }
 
 int main()
 {
     // kymchi lines
-    cin.tie( 0 ); cout.tie( 0 ); ios_base::sync_with_stdio( false );
+    kymchi
     
     int* fibo = new int[10];
     
     generate_fibo( fibo );
     
+    // print fibo
     for ( int i = 0; i < 10; i++ )
         cout << i[fibo] << " ";
     
-    krys_free( fibo );
+    // dont leak memory
+    // i mean you can leak it if you want
+    krystal_free( fibo );
 }
